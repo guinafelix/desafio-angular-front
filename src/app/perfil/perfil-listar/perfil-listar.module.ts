d46bc5e4a352c0240750516598f9cf8b13efc6ce
guinafelix <oguilhermeleitefelix@gmail.com>
2023-08-12 08:51:07 -0300

    feat: add toolbar

--- src/app/perfil/perfil-listar/perfil-listar.module.ts
+++ src/app/perfil/perfil-listar/perfil-listar.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PerfilListarRoutingModule } from './perfil-listar-routing.module';
-
 import {MatTableModule} from '@angular/material/table';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { PerfilListarComponent } from './perfil-listar/perfil-listar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [PerfilListarComponent],
@@ -13,7 +13,8 @@ import { PerfilListarComponent } from './perfil-listar/perfil-listar.component';
     CommonModule,
     PerfilListarRoutingModule,
     MatTableModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatToolbarModule
   ]
 })
 export class PerfilListarModule { }
