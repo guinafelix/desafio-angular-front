55a04759cd18226488d82f8fd17442dcec6f1c2d
guinafelix <oguilhermeleitefelix@gmail.com>
2023-08-12 13:50:29 -0300

    feat: add routes.ts

---
+++ src/app/routes.ts
@@ -0,0 +1,33 @@
+import { HomeComponent } from './home/home.component';
+
+export default [
+  {path:"", component:HomeComponent},
+  {
+    path:"perfis",
+    loadChildren: () => import('./perfil/perfil-listar/perfil-listar.module').then(m => m.PerfilListarModule) 
+  },
+  {
+    path: "perfil/cadastrar",
+    loadChildren: () => import('./perfil/perfil-cadastrar-editar/perfil-cadastrar-editar.module').then(m => m.PerfilCadastrarEditarModule)
+  },
+  {
+    path: "perfil/editar/:id",
+    loadChildren: () => import('./perfil/perfil-cadastrar-editar/perfil-cadastrar-editar.module').then(m => m.PerfilCadastrarEditarModule)
+  }, 
+  {
+    path: "disciplinas",
+    loadChildren: () => import('./disciplina/disciplina-listar/disciplina-listar.module').then(m => m.DisciplinaListarModule)
+  },
+  {
+    path: "disciplina/cadastrar",
+    loadChildren: () => import('./disciplina/disciplina-cadastrar-editar/disciplina-cadastrar-editar.module').then(m => m.DisciplinaCadastrarEditarModule)
+  },
+  {
+    path: "disciplina/editar/:id",
+    loadChildren: () => import('./disciplina/disciplina-cadastrar-editar/disciplina-cadastrar-editar.module').then(m => m.DisciplinaCadastrarEditarModule)
+  },
+  {
+    path: "cursos",
+    loadChildren: () => import('./curso/curso-listar/curso-listar.module').then(m => m.CursoListarModule) 
+  }
+]
