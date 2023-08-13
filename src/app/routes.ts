09eb8e461ee4b54022b13dd470bafac3c0353fc6
guinafelix <oguilhermeleitefelix@gmail.com>
2023-08-13 16:10:31 -0300

    refactor: adjut home component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,53 +1,58 @@
 import { HomeComponent } from './home/home.component';
 
 export default [
-  {path:"", component:HomeComponent},
   {
-    path:"perfis",
-    loadChildren: () => import('./perfil/perfil-listar/perfil-listar.module').then(m => m.PerfilListarModule) 
+    path:"", 
+    component:HomeComponent, 
+    children:[
+      {
+        path:"perfis",
+        loadChildren: () => import('./perfil/perfil-listar/perfil-listar.module').then(m => m.PerfilListarModule) 
+      },
+      {
+        path: "perfil/cadastrar",
+        loadChildren: () => import('./perfil/perfil-cadastrar-editar/perfil-cadastrar-editar.module').then(m => m.PerfilCadastrarEditarModule)
+      },
+      {
+        path: "perfil/editar/:id",
+        loadChildren: () => import('./perfil/perfil-cadastrar-editar/perfil-cadastrar-editar.module').then(m => m.PerfilCadastrarEditarModule)
+      }, 
+      {
+        path: "disciplinas",
+        loadChildren: () => import('./disciplina/disciplina-listar/disciplina-listar.module').then(m => m.DisciplinaListarModule)
+      },
+      {
+        path: "disciplina/cadastrar",
+        loadChildren: () => import('./disciplina/disciplina-cadastrar-editar/disciplina-cadastrar-editar.module').then(m => m.DisciplinaCadastrarEditarModule)
+      },
+      {
+        path: "disciplina/editar/:id",
+        loadChildren: () => import('./disciplina/disciplina-cadastrar-editar/disciplina-cadastrar-editar.module').then(m => m.DisciplinaCadastrarEditarModule)
+      },
+      {
+        path: "cursos",
+        loadChildren: () => import('./curso/curso-listar/curso-listar.module').then(m => m.CursoListarModule) 
+      },
+      {
+        path: "curso/matriz/:id",
+        loadChildren: () => import('./curso/curso-listar-matriz/curso-listar-matriz.module').then(m => m.CursoListarMatrizModule)
+      },
+      {
+        path: "curso/cadastrar",
+        loadChildren: () => import('./curso/curso-cadastrar-editar/curso-cadastrar-editar.module').then(m => m.CursoCadastrarEditarModule)
+      },
+      {
+        path:"usuarios",
+        loadChildren: () => import('./usuario/usuario-listar/usuario-listar.module').then(m => m.UsuarioListarModule)
+      },
+      {
+        path:"usuario",
+        loadChildren: () => import('./usuario/usuario-cadastrar-editar/usuario-cadastrar-editar.module').then(m => m.UsuarioCadastrarEditarModule)
+      },
+      {
+        path:"usuario",
+        loadChildren: () => import('./usuario/usuario-cadastrar-editar/usuario-cadastrar-editar.module').then(m => m.UsuarioCadastrarEditarModule)
+      }
+    ]
   },
-  {
-    path: "perfil/cadastrar",
-    loadChildren: () => import('./perfil/perfil-cadastrar-editar/perfil-cadastrar-editar.module').then(m => m.PerfilCadastrarEditarModule)
-  },
-  {
-    path: "perfil/editar/:id",
-    loadChildren: () => import('./perfil/perfil-cadastrar-editar/perfil-cadastrar-editar.module').then(m => m.PerfilCadastrarEditarModule)
-  }, 
-  {
-    path: "disciplinas",
-    loadChildren: () => import('./disciplina/disciplina-listar/disciplina-listar.module').then(m => m.DisciplinaListarModule)
-  },
-  {
-    path: "disciplina/cadastrar",
-    loadChildren: () => import('./disciplina/disciplina-cadastrar-editar/disciplina-cadastrar-editar.module').then(m => m.DisciplinaCadastrarEditarModule)
-  },
-  {
-    path: "disciplina/editar/:id",
-    loadChildren: () => import('./disciplina/disciplina-cadastrar-editar/disciplina-cadastrar-editar.module').then(m => m.DisciplinaCadastrarEditarModule)
-  },
-  {
-    path: "cursos",
-    loadChildren: () => import('./curso/curso-listar/curso-listar.module').then(m => m.CursoListarModule) 
-  },
-  {
-    path: "curso/matriz/:id",
-    loadChildren: () => import('./curso/curso-listar-matriz/curso-listar-matriz.module').then(m => m.CursoListarMatrizModule)
-  },
-  {
-    path: "curso/cadastrar",
-    loadChildren: () => import('./curso/curso-cadastrar-editar/curso-cadastrar-editar.module').then(m => m.CursoCadastrarEditarModule)
-  },
-  {
-    path:"usuarios",
-    loadChildren: () => import('./usuario/usuario-listar/usuario-listar.module').then(m => m.UsuarioListarModule)
-  },
-  {
-    path:"usuario",
-    loadChildren: () => import('./usuario/usuario-cadastrar-editar/usuario-cadastrar-editar.module').then(m => m.UsuarioCadastrarEditarModule)
-  },
-  {
-    path:"usuario",
-    loadChildren: () => import('./usuario/usuario-cadastrar-editar/usuario-cadastrar-editar.module').then(m => m.UsuarioCadastrarEditarModule)
-  }
 ]
