f2adfa44d39f2d0b157c8abe24f3d62a0eeeec64
guinafelix <oguilhermeleitefelix@gmail.com>
2023-08-13 21:39:20 -0300

    feat: add logout function

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -11,9 +12,17 @@ import { map, shareReplay } from 'rxjs/operators';
 export class MainNavComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
+  constructor(private router: Router) {}
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+
+  logout() {
+    console.log('logout')
+    localStorage.removeItem('token')
+    this.router.navigateByUrl('/login')
+  }
 }
